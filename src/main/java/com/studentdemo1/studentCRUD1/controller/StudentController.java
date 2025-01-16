6dc3316f2ef1a728f5bf113f1f91ffc5603943ee
Visweshvar-UST <visweshvar.ust@gmail.com>
2025-01-16 17:16:39 +0530

    controller 01

---
+++ src/main/java/com/studentdemo1/studentCRUD1/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.studentdemo1.studentCRUD1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class StudentController {
+
+}
