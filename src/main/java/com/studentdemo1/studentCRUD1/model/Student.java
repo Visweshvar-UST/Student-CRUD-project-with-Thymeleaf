4b7e1fa7a2bc1e51e9a5f0e8d3aef1046193fa5d
Visweshvar-UST <visweshvar.ust@gmail.com>
2025-01-16 17:44:04 +0530

    Student.java getter and setter

--- src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
+++ src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
@@ -8,9 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
 @Entity
 public class Student {
     @Id
@@ -19,4 +19,36 @@ public class Student {
     String name;
     int age;
 
+    public Student() {
+    }
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
