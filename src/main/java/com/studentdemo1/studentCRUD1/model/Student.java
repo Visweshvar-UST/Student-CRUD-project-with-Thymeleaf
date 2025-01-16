a47e12b61f9a93b129b1ef44577cb67cc8e01905
Visweshvar-UST <visweshvar.ust@gmail.com>
2025-01-16 17:11:06 +0530

    model 01

--- src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
+++ src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
@@ -1,8 +1,22 @@
 package com.studentdemo1.studentCRUD1.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class Student {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    int age;
+
 }
