27014a741df3f77c24429e1b3e813052a02d861d
Visweshvar-UST <visweshvar.ust@gmail.com>
2025-01-17 10:57:32 +0530

    complete removal of JPA and DB

--- src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
+++ src/main/java/com/studentdemo1/studentCRUD1/model/Student.java
@@ -1,20 +1,20 @@
 package com.studentdemo1.studentCRUD1.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
 //@NoArgsConstructor
 //@AllArgsConstructor
 //@Data
-@Entity
+// @Entity
 public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String name;
     int age;
--- src/main/java/com/studentdemo1/studentCRUD1/repository/StudentRepo.java
+++ src/main/java/com/studentdemo1/studentCRUD1/repository/StudentRepo.java
@@ -1,10 +1,10 @@
 package com.studentdemo1.studentCRUD1.repository;
 
-import com.studentdemo1.studentCRUD1.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import com.studentdemo1.studentCRUD1.model.Student;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StudentRepo extends JpaRepository<Student,Long> {
+// @Repository
+public interface StudentRepo /*extends JpaRepository<Student,Long>*/ {
 }
 
